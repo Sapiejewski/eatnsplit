66f0b04ff670a70de754a3a3f26631c20aa5522f
Filip <fsapiejewski01@gmail.com>
2023-07-12 14:08:17 +0200

    first static vers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,72 @@
-
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 function App() {
   return (
-    <div className="App">
-      <p>xD</p>
-     </div>
+    <div className='app'>
+      <div className="sidebar">
+        <FriendsList/>
+        <FormAddFriend/>
+        <Button>Add friend</Button>
+      </div>
+    </div>
+  );
+}
+function FriendsList()
+{
+  const friends = initialFriends;
+  return (<ul>
+    {friends.map((friend)=> <Friend friend={friend} key={friend.id}/>)}
+    </ul>
+  );
+}
+export default App;
+function Friend({friend})
+{
+  return (
+    <li>
+      <img src={friend.img} alt={friend.name}></img>
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (<p className="red">You owe {friend.name} {Math.abs(friend.balance)}$</p>) }
+      {friend.balance > 0 && (<p className="green">{friend.name} owes you  {Math.abs(friend.balance)}$</p>) }
+      {friend.balance == 0 && (<p >You and {friend.name} are even</p>) }
+      <Button>Select</Button>
+    </li>
   );
 }
 
-export default App;
+function FormAddFriend(){
+  return(
+    <form className="form-add-friend"> 
+      <label>Friend name</label>
+      <input type='text'></input>
+
+      <label>Img url</label>
+      <input type='text'></input>
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function Button({children})
+{
+  return <button className="button">{children}</button>
+}
